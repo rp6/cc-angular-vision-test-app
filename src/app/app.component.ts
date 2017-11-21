a5b0eaa8809a0d82315579ca405c2cfdb484dce1
Roland Puhwein <roland.puhwein@ebcont.com>
2017-11-21 13:28:02 +0100

    working aws service with http url, new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageUploadService } from './core/services/image-upload.service';
-import * as AWS from 'aws-sdk';
-import { Credentials } from 'aws-sdk';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/forkJoin'
 import 'rxjs/add/observable/of'
-import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -15,132 +11,38 @@ import { Observable } from "rxjs/Observable";
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  file: any;
   response: any = {};
 
-  constructor(private imageUploadService: ImageUploadService, private httpClient: HttpClient) {
+  constructor(private imageUploadService: ImageUploadService) {
 
   }
 
   ngOnInit(): void {
-    // this.processImageWithAws();
-    // this.processImage();
   }
 
-  processImage(): void {
-    this.imageUploadService.processImageWithAzure(this.azureSubscriptionKey, this.currentImageUrl).subscribe((res: any) => {
-        console.log('success res', res);
+  analyseImageWithAzure(): void {
+    this.imageUploadService.analyseImageWithAzure(this.currentImageUrl, this.azureSubscriptionKey).subscribe((res: any) => {
         this.response = res;
       },
       (res: any) => {
-        console.log('error res', res);
-      })
+        this.response = res;
+      }
+    )
   }
 
-  onChange(event) {
-    const files = event.srcElement.files;
-    this.file = event.srcElement.files[0];
-    console.log(files);
-  }
-
-  getBase64(file): void {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = () => {
-      console.log(reader.result);
-      this.awsStuff(reader.result);
-    };
-    reader.onerror = (error) => {
-      console.log('Error: ', error);
-    };
-  }
-
-
-  // test(): void {
-  //   const file = $('#load-file')[0].files[0];
-  //   const fileReader = new FileReader();
-  //   fileReader.onloadend = function (e) {
-  //     const arrayBuffer = e.target.result;
-  //     blobUtil.arrayBufferToBlob(arrayBuffer, 'image/png').then(function (blob) {
-  //       console.log('here is a blob', blob);
-  //       console.log('its size is', blob.size);
-  //       console.log('its type is', blob.type);
-  //     }).catch(console.log.bind(console));
-  //   };
-  //   fileReader.readAsArrayBuffer(file);
-  // }
-
-  awsStuff(imageBlob): any {
-    const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27', region: this.awsRegion});
-    rekognition.config.credentials = new Credentials(this.awsAccessKey, this.awsSecretKey);
-    rekognition.detectLabels({
-        Image: {
-          Bytes: imageBlob
-        },
+  analyseImageWithAws(): void {
+    this.imageUploadService.analyseImageWithAWS(this.currentImageUrl, this.awsRegion, this.awsAccessKey, this.awsSecretKey).subscribe(
+      (res: any) => {
+        this.response = res;
       },
-      (error, data) => {
-        this.response = data;
-        console.log('test', error);
-      },
-
-    );
-    // return this.httpClient.get(this.currentImageUrl, {responseType: 'blob'})
-    //   .switchMap((data: Blob) =>
-    //     new Promise((resolve, reject) => {
-    //       const fileReader = new FileReader();
-    //       fileReader.onload = () => resolve(fileReader.result);
-    //       fileReader.onerror = reject;
-    //       fileReader.readAsArrayBuffer(data);
-    //     })
-    //   )
-    //   .switchMap((data: ArrayBuffer) =>
-    //     Observable.of(
-    //       rekognition.detectLabels({
-    //         Image: {Bytes: data}
-    //       }).promise()
-    //     ));
+      (res: any) => {
+        this.response = res;
+      }
+    )
   }
 
-  processImageWithAws(): void {
-    this.getBase64(this.file);
-
-    // this.awsStuff().subscribe(
-    //   (res: any) => {
-    //     console.log('res suc', res);
-    //   },
-    //   (res: any) => {
-    //     console.log('res error', res);
-    //   }
-    // )
-  }
-
-  processImageWithGoogle(): void {
-    const config = {
-      "requests": [
-        {
-          "image": {
-            "source": {
-              "imageUri":
-              this.currentImageUrl
-            }
-          },
-          "features": [
-            {
-              "type": "LABEL_DETECTION",
-              "maxResults": 1
-            }
-          ]
-        }
-      ]
-    };
-
-
-    const url = 'https://vision.googleapis.com/v1/images:annotate?key=' + this.googleApiKey;
-
-    const httpHeaders = new HttpHeaders();
-    httpHeaders.append('Content-Type', 'application/json');
-
-    this.httpClient.post(url, config, {headers: httpHeaders}).subscribe(
+  analyseImageWithGoogle(): void {
+    this.imageUploadService.analyseImageWithGoogle(this.currentImageUrl, this.googleApiKey).subscribe(
       (res) => {
         this.response = res;
       },
